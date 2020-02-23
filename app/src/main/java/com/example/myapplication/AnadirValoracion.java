cdffed932bcc4be8aed5d2a7f6210c79711421c8
Josu <josu_gutierrez@hotmail.com>
2020-02-23 20:21:16 +0100

    Vistas

---
+++ app/src/main/java/com/example/myapplication/AnadirValoracion.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnadirValoracion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anadir_valoracion);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/CambiarValoracion.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CambiarValoracion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambiar_valoracion);
+    }
+}
